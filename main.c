d40dd127f4f13114e5f182aa655e610ce5de081f
lucas <lucascampardo@outlook.com>
2024-02-25 14:59:05 -0300

    Implementando novas funcionalidades no jogo

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+// #include <math.h>
 
 int main() {
 
     // Imprime o cabeçalho do jogo
     printf("******************************\n");
     printf("* Bem vindo ao Guess Number! *\n");
-    printf("******************************");
+    printf("******************************\n");
+
+    int secNumber = 42;
+
+    int chute;
+
+    printf("Chute um numero: ");
+    scanf("%d", &chute);
+
+    if (chute == secNumber) {
+        printf("Parabens, voce acertou o numero secreto!");
+    } else {
+        printf("Tente novamente!");
+    }
+
 }
