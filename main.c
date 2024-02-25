2544631c27cd1c10e50f88e55d1286f229f3c8f7
lucas <lucascampardo@outlook.com>
2024-02-25 13:06:03 -0300

    Iniciando o projeto em C

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    // Imprime o cabeçalho do jogo
+    printf("******************************\n");
+    printf("* Bem vindo ao Guess Number! *\n");
+    printf("******************************");
+}
